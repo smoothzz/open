faff47eb5bda5d6866bb02669193330453900987
smoothzz <53614138+smoothzz@users.noreply.github.com>
2021-04-06 13:01:43 -0300

    Update prepare-rke.sh

--- prepare-rke.sh
+++ prepare-rke.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 ufw disable
 swapoff -a; sed -i '/swap/d' /etc/fstab
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sysctl --system
+systemctl --system
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
