198315dac86f2a13a67476ba7d4ffab5f4d711ab
smoothzz <thiago.rdoa@outlook.com>
2021-05-04 17:30:53 -0300

    new file ubuntu k8s

---
+++ shellscript/k8s-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update -y
+curl -fsSL https://get.docker.com | bash
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload
+systemctl restart docker
+apt-get update && apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
